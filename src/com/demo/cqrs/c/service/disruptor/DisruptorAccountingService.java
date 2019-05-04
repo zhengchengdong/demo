48de14a33104250856bf1bbe0ab24a9a7ad381f1
neo <neo@DESKTOP-ITH4FG3>
2019-05-04 22:48:36 +0800

    完成初始功能

--- src/com/demo/cqrs/c/service/disruptor/DisruptorAccountingService.java
+++ src/com/demo/cqrs/c/service/disruptor/DisruptorAccountingService.java
@@ -3,15 +3,22 @@ package com.demo.cqrs.c.service.disruptor;
 import com.demo.cqrs.c.service.AccountingService;
 import com.demo.cqrs.c.service.impl.AccountingServiceImpl;
 import com.demo.cqrs.c.service.result.TransferResult;
+import com.matrix.disruptor.DisruptorServiceBase;
 
-public class DisruptorAccountingService implements AccountingService {
+public class DisruptorAccountingService extends DisruptorServiceBase implements AccountingService {
+
+	DisruptorAccountingService(AccountingServiceImpl accountingServiceImpl) {
+		super(accountingServiceImpl);
+		this.accountingServiceImpl = accountingServiceImpl;
+	}
 
 	private AccountingServiceImpl accountingServiceImpl;
 
 	@Override
 	public TransferResult transfer(String fromAccountId, String toAccountId, Integer amount) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return executeFunction(DisruptorNames.accounting.name(), () -> {
+			return accountingServiceImpl.transfer(fromAccountId, toAccountId, amount);
+		});
 	}
 
 }
---
+++ src/com/demo/cqrs/c/service/disruptor/DisruptorNames.java
@@ -0,0 +1,5 @@
+package com.demo.cqrs.c.service.disruptor;
+
+public enum DisruptorNames {
+	accounting
+}
